dd5ece3dccc859df421dbb7669d567912e30915a
CristianAguileraMontenegro <cristian.aguilera.m@mail.pucv.cl>
2022-06-20 22:11:08 -0400

    validar

--- Classes/terminalVeterinario.py
+++ Classes/terminalVeterinario.py
@@ -2,26 +2,71 @@
 from tkinter import *
 import os
 import os.path
+from unicodedata import name
 import uuid
 
-from terminalVeterinario import TerminalVeterinario
+
+import os
+import os.path
+import uuid
+import mysql.connector
+
+
+db = mysql.connector.connect(
+    user='root',
+    password='root',
+    host='localhost',
+    database='mydb',
+    port='3306'
+)
+
+mycursor = db.cursor()
+mycursor.execute('select * from fichaMedica')
+
+
+resultado = mycursor.fetchone()
+
+#import para el uso de la interfaz grafica de qt
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+#from terminalVeterinario import TerminalVeterinario
 from tablaMedica import TablaMedica
 from mascota import Mascota
 
-class TerminalVeterinario:
+class TerminalVeterinario(QMainWindow):
 
     def __init__(self, id : uuid, tokenActivacion, idVeterinaria :uuid, nombreVeterinaria):
+        super().__init__()
+        uic.loadUi("Proyecto-PetRecord\Complementos\GUIAPP_keyInsert.ui", self)
+        self.botonConfirmarkey.clicked.connect(self.validarLlaveConServidor) #metodos de botones en el contrusctor
         self.id = id
         self.tokenActivacion = tokenActivacion
         self.idVeterinaria = idVeterinaria
         self.nombreVeterinaria = nombreVeterinaria
         self.mascotas = [Mascota]
+       
+
 
     def validarConexionInternet(self):
         pass
 
     def validarLlaveConServidor(self):
-        pass
+
+        #uic.loadUi("Proyecto-PetRecord/Complementos/AbstracMedico.ui", self) 
+
+        llaveEntrada = self.keyInput.text()##obtiene los datos ingresados de tiene que ponder en nombre de la clase 
+        #toma los valores como string
+        llaveBaseDeDatos = mycursor.execute(f'SELECT Llaves FROM keysactivacion WHERE Llaves = {llaveEntrada}')
+        resultado = mycursor.fetchall()
+        print(resultado)
+
+        if(llaveEntrada == resultado[0][0]):
+            print("coincidencia")
+
+        ##esto es prueba local para ver como sacar los datos cambiar a con el servidor
 
     def activarTokenDeActivacion(self):
         pass
@@ -73,35 +118,10 @@ class TerminalVeterinario:
                 idRand = uuid.uuid4()
                 print(idRand)
                 f.write(f"id = {idRand}")
-                
-root = Tk()
-root.title('My Pet Record :)')
-root.geometry("1280x720")
-root.configure(background="#2C794B")
-
-#Crear campos de entrada
-alergias = Entry(root, width=30)
-alergias.grid(row=0, column=1)
-registro_Operaciones = Entry(root, width=30)
-registro_Operaciones.grid(row=1, column=1)
-vacunas_Suministradas = Entry(root, width=30)
-vacunas_Suministradas.grid(row=2, column=1)
-
-#Crear frame para fichas
-fichas_Frame = LabelFrame(root, text="Fichas médicas mascota",  padx=25, pady=25)
-fichas_Frame.grid(row=3, column=0, padx=10, pady=10)
-
-#Crear boton de agrear ficha
-btt_Agregar_Ficha = Button(fichas_Frame, text="Agregar Ficha")
-btt_Agregar_Ficha.pack()
-
-#Crear labels de los campos de entrada
-alergias_Label = Label(root, text="Alergias :")
-alergias_Label.grid(row=0, column=0)
-registro_Operaciones_Label = Label(root, text="Registro Operaciones")
-registro_Operaciones_Label.grid(row=1, column=0)
-vacunas_Suministradas_Label = Label(root, text="Vacunas Suministradas")
-vacunas_Suministradas_Label.grid(row=2, column=0, padx=50)
-
-root.mainloop()
+              
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    GUI = TerminalVeterinario(12,"true","1","PetLife")#datos de prueba
+    GUI.show()
+    sys.exit(app.exec_())
 
