a0966fb7e64f968a8879a9f308411874225f64c9
pieroBri <pierobri36@gmail.com>
2022-06-26 20:41:32 -0400

    Co-authored-by: Deline37 <Deline37@users.noreply.github.com>

--- Classes/terminalVeterinario.py
+++ Classes/terminalVeterinario.py
@@ -4,12 +4,13 @@ import os
 import os.path
 import uuid
 
+
 import mysql.connector
 
 
 db = mysql.connector.connect(
-    user='root',
-    password='root',
+    user='piero',
+    password='pieron123',
     host='localhost',
     database='mydb',
     port='3306'
@@ -24,7 +25,7 @@ resultado = mycursor.fetchone()
 #import para el uso de la interfaz grafica de qt
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import *
 
 
 #from terminalVeterinario import TerminalVeterinario
@@ -32,29 +33,63 @@ from tablaMedica import TablaMedica
 from mascota import Mascota
 
 class TerminalVeterinario(QMainWindow):
-
     def __init__(self):
         super().__init__()
-        uic.loadUi("Complementos\GUIAPP_keyInsert.ui", self)
-        self.botonConfirmarkey.clicked.connect(self.validarLlaveConServidor) #metodos de botones en el constructor
+        # uic.loadUi("Complementos\GUIAPP_keyInsert.ui", self)
+        # self.botonConfirmarkey.clicked.connect(self.validarLlaveConServidor) #metodos de botones en el constructor
         
         self.id = self.generarIdTerminal()
-        self.tokenActivacion = self.validarTokenDeActivacion()
+        self.tokenActivacion = None
+        self.mascotas = []
+        self.validarTokenDeActivacion()
+        self.nombreVeterinaria = None
         self.idVeterinaria = None
-        self.nombreVeterinaria =  None
-        self.mascotas = [Mascota]
-       
-
+        if (self.tokenActivacion == True):
+            self.setIdVeterinaria()
+            self.setNombreVeterinaria()
+            self.setMascotas()
+            
+            
+        
 
     def validarConexionInternet(self):
         pass
         
-    def setIdVeterinaria(self, idVet):
-        self.idVeterinaria = idVet
+    def setIdVeterinaria(self):
+        sql = 'SELECT Veterinaria_idVeterinaria FROM TerminalVeterinario WHERE idTerminalVeterinario = (%s)'
+        mycursor.execute(sql, (str(self.id),))
+        idVet = mycursor.fetchone()
+        self.idVeterinaria = idVet[0]
 
-    def setNombreVeterinaria(self, nombreVet):
-        self.nombreVeterinaria = nombreVet    
+    def setNombreVeterinaria(self):
+        sql = 'SELECT Veterinaria_nombreVeterinaria FROM TerminalVeterinario WHERE idTerminalVeterinario = (%s)'
+        mycursor.execute(sql, (str(self.id),))
+        nombreVet = mycursor.fetchone()
+        self.nombreVeterinaria = nombreVet[0]    
     
+    def setMascotas(self):
+        sql = 'SELECT Mascota_idMascota FROM Mascota_has_TerminalVeterinario WHERE TerminalVeterinario_Veterinaria_idVeterinaria = (%s)'
+        mycursor.execute(sql, (str(self.idVeterinaria),))
+        ids = mycursor.fetchall()
+        for i in range(len(ids)):
+            sql = 'SELECT * FROM Mascota WHERE idMascota = (%s)'
+            mycursor.execute(sql, (str(ids[i][0]),))
+            resultado = mycursor.fetchone()
+            sql = 'SELECT * FROM RegistroDeOperaciones WHERE TablaMedica_idTablaMedica = (%s)'
+            mycursor.execute(sql, (str(resultado[9]),))
+            registroOp = mycursor.fetchall()
+            sql = 'SELECT * FROM RegistroVacunasSuministradas WHERE TablaMedica_idTablaMedica = (%s)'
+            mycursor.execute(sql, (str(resultado[9]),))
+            registroVac = mycursor.fetchall()
+            sql = 'SELECT * FROM Alergias WHERE TablaMedica_idTablaMedica = (%s)'
+            mycursor.execute(sql, (str(resultado[9]),))
+            alergiasEntregar = mycursor.fetchall()
+            tablaEntregar = TablaMedica(resultado[9],  alergiasEntregar, registroOp, registroVac)
+            mascotalol= Mascota(resultado[0], resultado[1], resultado[2], resultado[3], resultado[4], resultado[5], resultado[6],
+                                    resultado[7], resultado[8], tablaEntregar)
+            self.mascotas.append(mascotalol)
+            
+            
     def validarLlaveConServidor(self):
 
         #uic.loadUi("Proyecto-PetRecord/Complementos/AbstracMedico.ui", self) 
@@ -65,6 +100,7 @@ class TerminalVeterinario(QMainWindow):
         resultado = mycursor.fetchone()
         
         if(resultado == None):
+            self.label_3.setVisible(True)
             self.validarLlaveConServidor()
         else:
             mycursor.execute(f'SELECT Veterinaria_idVeterinaria FROM Keysactivación WHERE Llaves = {llaveEntrada}')
@@ -84,7 +120,7 @@ class TerminalVeterinario(QMainWindow):
         self.setNombreVeterinaria(nombreVet)
         uic.loadUi("Complementos/buscarMascota.ui",self)
 
-    def validarTokenDeActivacion(self)->bool :
+    def validarTokenDeActivacion(self):
         
         sql = 'SELECT tokenDeActivación FROM terminalveterinario WHERE idTerminalVeterinario = (%s)'
         val = (self.id)
@@ -94,32 +130,125 @@ class TerminalVeterinario(QMainWindow):
         if(resultado == None):
             uic.loadUi("Complementos/GUIAPP_KeyInsert.ui",self)
             self.botonConfirmarkey.clicked.connect(self.validarLlaveConServidor)
-        elif (resultado[0] == 1):
+        elif(resultado[0] == 1):
+            self.tokenActivacion = True
             uic.loadUi("Complementos/buscarMascota.ui", self)
+            self.BotonBuscar.clicked.connect(self.verificarMascotaEnSistema)
+            
 
     def ingresarMascotaAlSistema(self, mascotaNueva: list):
         self.mascotas.append(mascotaNueva)
         mycursor.execute('')
 
-    def verificarMascotaEnSistema(self, idMascota):
-        for i in self.mascotas:
-            if(i.id == idMascota):
-                return True
-        return False
+    def verificarMascotaEnSistema(self):
+        idMascotaBuscada = self.inputBuscar.text()
+        sql = 'SELECT idMascota FROM mascota WHERE idMascota = (%s)'
+        mycursor.execute(sql, (idMascotaBuscada,))
+        resultado = mycursor.fetchone()
+        if(resultado != None):
+            sql = 'SELECT Mascota_idMascota FROM Mascota_has_TerminalVeterinario WHERE TerminalVeterinario_Veterinaria_idVeterinaria = (%s)'
+            mycursor.execute(sql, (self.idVeterinaria,))
+            resultado = mycursor.fetchall()
+            flagMascotaEnc = False
+            for id in resultado:
+                if(id[0] == str(idMascotaBuscada)):
+                    flagMascotaEnc = True
+            self.botonAgregar.setVisible(False)
+            if(flagMascotaEnc):
+                sql = 'SELECT * FROM mascota WHERE idMascota = (%s)'
+                mycursor.execute(sql, (idMascotaBuscada,))
+                resultado2 = mycursor.fetchone()
+                self.datosMostrar.setText('Codigo mascota: ' + str(resultado2[0]) + ', Nombre mascota: ' 
+                + str(resultado2[1]) + ', Especie: ' + str(resultado2[2]) + ', Raza: ' + str(resultado2[4]) + ', Dueño/a: ' + str(resultado2[5]))
+                self.datosMostrar.setVisible(True)
+                self.botonEntrar.setVisible(True)
+                self.botonAbstracto.setVisible(False)
+                for mascota in self.mascotas:
+                    if(mascota.id == idMascotaBuscada):
+                        mascotaMostrada = mascota
+                self.botonEntrar.clicked.connect(lambda : self.verScreenDatosTotal(mascotaMostrada))
+            else:
+                sql = 'SELECT * FROM mascota WHERE idMascota = (%s)'
+                mycursor.execute(sql, (idMascotaBuscada,))
+                resultado2 = mycursor.fetchone()
+                self.datosMostrar.setText('Codigo mascota: ' + str(resultado2[0]) + ', Nombre mascota: ' 
+                + str(resultado2[1]) + ', Especie: ' + str(resultado2[2]) + ', Raza: ' + str(resultado2[4]) + ', Dueño/a: ' + str(resultado2[5]))
+                self.datosMostrar.setVisible(True)
+                self.botonAbstracto.setVisible(True)
+                self.botonEntrar.setVisible(False)
+        else:
+            #Funcion registrar mascota
+            self.datosMostrar.setText('Mascota no ingresada en el sistema')
+            self.botonAgregar.setVisible(True)
+            self.datosMostrar.setVisible(True)
+            self.botonAbstracto.setVisible(False)
+            self.botonEntrar.setVisible(False)
+            self.llenarInfoBasicaMascota(idMascotaBuscada)
 
-    def llenarinfoBasicaMascota(self, ):
+    def verScreenDatosTotal(self, mascotaMostrada):
+        alergias = ''
+        operaciones = ''
+        vacunas = ''
+        uic.loadUi("Complementos/VistaDatosMascotaTotal.ui", self)
+        value = self.infoBasicaMascota.item(0)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.nombre))
+        value = self.infoBasicaMascota.item(1)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.especie))
+        value = self.infoBasicaMascota.item(2)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.raza))
+        value = self.infoBasicaMascota.item(3)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.color))
+        value = self.infoBasicaMascota.item(4)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.rutTutor))
+        value = self.infoBasicaMascota.item(5)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.numeroTelefono))
+        value = self.infoBasicaMascota.item(6)
+        value.setText(value.text() + '  '+ str(mascotaMostrada.direccion))
+
+        value2 = self.datoTablaMedica.item(0)
+        for i in range(len(mascotaMostrada.tablaMedica.alergias)):
+            alergias = alergias + str(mascotaMostrada.tablaMedica.alergias[i][1])
+        value2.setText(value2.text() + '  '+ alergias)
+        
+        value2 = self.datoTablaMedica.item(1)
+        for i in range(len(mascotaMostrada.tablaMedica.registroDeOperaciones)):
+            operaciones = operaciones + str(mascotaMostrada.tablaMedica.registroDeOperaciones[i][1])
+        value2.setText(value2.text() + '  '+ operaciones)
+
+        value2 = self.datoTablaMedica.item(2)
+        for i in range(len(mascotaMostrada.tablaMedica.vacunasSuministradas)):
+            vacunas = vacunas + str(mascotaMostrada.tablaMedica.vacunasSuministradas[i][1])
+        value2.setText(value2.text() + '  '+ vacunas)
+
+        boton = QLabel('LOL click', self)
+        boton.move(10, 10)
+        
+    def llenarInfoBasicaMascota(self, idMascota):
+        # self.ingresarMascotaAlSistema(mascotaEnviada)
         pass
     
-    def editarInfoBasicaMascota():
+    def editarInfoBasicaMascota(self, idMascota):
+        for mascota in self.mascotas:
+            if(idMascota == mascota):
+                mascota.editarInfoBasicaMascota()
         pass
 
-    def buscarFichaMedica():
+    def buscarFichaMedica(self, idMascota):
+        for mascota in self.mascotas:
+            if(idMascota == mascota):
+                mascota.buscarFichaMedica()
         pass
     
-    def crearFichaMedicaConsulta():
+    def crearFichaMedicaConsulta(self, idMascota):
+        for mascota in self.mascotas:
+            if(idMascota == mascota):
+                mascota.crearFichaMedicaConsulta()
         pass
 
-    def editarFichaMedicaConsulta():
+    def editarFichaMedicaConsulta(self, idMascota):
+        for mascota in self.mascotas:
+            if(idMascota == mascota):
+                mascota.editarFichaMedicaConsulta()
         pass
 
     def GenerarConexiónServidor():
